c2262422e9bfe15f8a0d013f4d3e9097cd3b09be
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-11-11 22:13:55 +0200

    fox1

--- js/Truthy.js
+++ js/Truthy.js
@@ -8,7 +8,21 @@
 // console.log(age < 10 || age > 30);
 
 // Розгалуження
-const arr = [1, 23, 44, 55, 7];
+// const arr = [1, 23, 44, 55, 7];
 
-arr.splice(3, 3);
-console.log(arr);
+// arr.splice(3, 3);
+// console.log(arr);
+
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  rating: 8.38,
+};
+
+for (const key in book) {
+  // Ключ
+  console.log(key);
+  // Значення властивості з таким ключем
+  console.log(book[key]);
+}
